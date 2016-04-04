e7e96cde795ba33ce1a825880d0e901967e522a0
jacob.ege <jacob.ege@gmail.com>
2016-04-04 20:43:02 +0200

    DateTest

--- test/Exercise_1_test/DateTest.java
+++ test/Exercise_1_test/DateTest.java
@@ -1,17 +1,8 @@
 package Exercise_1_test;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import Exercise_1.ArgumentException;
 import Exercise_1.Date;
 import Exercise_1.InvalidDateException;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -24,73 +15,73 @@ public class DateTest {
     public DateTest() {
     }
 
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
     @Test
-    public void NextDateTest_MinimumGoodBoundary() throws InvalidDateException, ArgumentException {
+    public void NextDateTest_MinimumGoodBoundary() {
         Date result = new Date(1, 1, 1812);
-        result.nextDate();
+        try {
+            result = result.nextDate();
+        } catch (ArgumentException | InvalidDateException e) {
+            System.out.println("Exception occured");
+            System.out.println(e.getMessage());
+        }
 
         Date expectedResult = new Date(1, 2, 1812);
         assertEquals(expectedResult, result);
     }
 
     @Test
-    public void NextDateTest_MaximumGoodBoundary() throws InvalidDateException, ArgumentException {
+    public void NextDateTest_MaximumGoodBoundary() {
         Date result = new Date(12, 31, 2014);
-        result.nextDate();
+        try {
+            result = result.nextDate();
+        } catch (InvalidDateException | ArgumentException e) {
+            System.out.println("Exception occured");
+            System.out.println(e.getMessage());
+        }
 
         Date expectedResult = new Date(1, 1, 2015);
         assertEquals(expectedResult, result);
     }
 
-    @Test
-    public void NextDateTest_MinimumBadBoundary_Month() throws ArgumentException, InvalidDateException {
+    @Test(expected=ArgumentException.class)
+    public void NextDateTest_MinimumBadBoundary_Month() throws InvalidDateException, ArgumentException {
         new Date(0, 1, 1812).nextDate();
     }
 
-    @Test
+    @Test(expected=ArgumentException.class)
     public void NextDateTest_MinimumBadBoundary_Day() throws ArgumentException, InvalidDateException {
         new Date(1, 0, 1812).nextDate();
     }
 
-    @Test
+    @Test(expected=ArgumentException.class)
     public void NextDateTest_MinimumBadBoundary_Year() throws ArgumentException, InvalidDateException {
         new Date(1, 1, 1811).nextDate();
     }
 
-    @Test
+    @Test(expected=ArgumentException.class)
     public void NextDateTest_MaximumBadBoundary_Month() throws ArgumentException, InvalidDateException {
         new Date(13, 12, 2014).nextDate();
     }
 
-    @Test
+    @Test(expected=ArgumentException.class)
     public void NextDateTest_MaximumBadBoundary_Day() throws ArgumentException, InvalidDateException {
         new Date(12, 32, 2014).nextDate();
     }
 
-    @Test
+    @Test(expected=ArgumentException.class)
     public void NextDateTest_MaximumBadBoundary_Year() throws ArgumentException, InvalidDateException {
         new Date(12, 31, 2015).nextDate();
     }
 
     @Test
-    public void NextDateTest_SpecialCase_LeapYear() throws InvalidDateException, ArgumentException {
-        Date result = new Date(2, 28, 2012).nextDate();
+    public void NextDateTest_SpecialCase_LeapYear() {
+        Date result = null;
+        try {
+            result = new Date(2, 28, 2012).nextDate();
+        } catch (InvalidDateException | ArgumentException e) {
+            System.out.println("Exception occured");
+            System.out.println(e.getMessage());
+        }
 
         Date expectedResult = new Date(2, 29, 2012);
 
@@ -98,12 +89,23 @@ public class DateTest {
     }
 
     @Test
-    public void NextDateTest_SpecialCase_CommonYear() throws InvalidDateException, ArgumentException {
-        Date result = new Date(2, 28, 2011).nextDate();
+    public void NextDateTest_SpecialCase_CommonYear() {
+        Date result = null;
+        try {
+            result = new Date(2, 28, 2011).nextDate();
+        } catch (InvalidDateException | ArgumentException e) {
+            System.out.println("Exception occured");
+            System.out.println(e.getMessage());
+        }
 
         Date expectedResult = new Date(3, 1, 2011);
 
         assertEquals(expectedResult, result);
     }
 
+    @Test(expected=InvalidDateException.class)
+    public void NextDateTest_InvalidDate() throws InvalidDateException, ArgumentException {
+        Date result = new Date(2, 29, 2011);
+        result.nextDate();
+    }
 }
