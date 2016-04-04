b486f07b7841e5705439357fc1b572c6bd56f9f3
Sean Emerson <sean.s.emerson@gmail.com>
2016-04-04 17:35:14 +0200

    new Genlist

--- test/Exercise_3_test/GenListXTest.java
+++ test/Exercise_3_test/GenListXTest.java
@@ -1,12 +1,10 @@
 package Exercise_3_test;
 
 import Exercise_3.GenListX;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GenListXTest {
     
